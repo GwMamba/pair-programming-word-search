04c364aa1a6397fbf66871df6ed34951d0207ad0
Gerald <gerald.mwang@gmail.com>
2023-11-12 20:29:37 -0700

    added transose/map function to check to find the word vertically.

--- wordsearch.js
+++ wordsearch.js
@@ -1,20 +1,25 @@
-
+// pair programming assignment with David Giroux and Gerald Mwangi
 
 const wordSearch = (letters, word) => {
   const horizontalJoin = letters.map(ls => ls.join(''));
+
 //   console.log("horizontal array", horizontalJoin);
   for (l of horizontalJoin) {
     if (l.includes(word)) return true;
   }
+
   // Transpose array to target vertical letters:
   const transpose = array => array[0].map((col, i) => array.map(row => row[i]));
   const transposedLetters = transpose(letters);
+
 //   console.log("Tranposed array", transposedLetters);
   const verticalJoin = transposedLetters.map(ls => ls.join(''));
+
 //   console.log(verticalJoin);
   for (l of verticalJoin) {
     if (l.includes(word)) return true;
   }
+//   If word not found horizontal or vertical; return false:
   return false;
 
 };
